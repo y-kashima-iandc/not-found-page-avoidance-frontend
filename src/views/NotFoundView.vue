8d5a30650beed0675aa377de3660057c9888ab88
y-kashima <y-kashima@iandcinc.co.jp>
2024-04-11 09:40:02 +0900

    Initial commit

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+// エラー画面がインデックス登録されないようにする
+document.querySelector("meta[name='robots']")?.setAttribute("content", "noindex");
+</script>
+
+<template>
+  <div class="notfound">
+    <h1>This is a 404(not found) page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .notfound {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
